f7b746ff25753fd867873daded8e15b0c23391e9
amaniq88 <amani.musallamq88@gmail.com>
2022-01-28 08:58:07 +0200

    Recursion all done

--- recursion/recursion.test.js
+++ recursion/recursion.test.js
@@ -1,9 +1,15 @@
 /* Write a function to do the division operation without using the built-in division*/
 
-function division(number, dividedBy){
-    // Write you logic here.
-    return;
+function division(n, m){
+    if (n < m )
+        return 0;
+        else
+        return 1 + division((n - m) , m); 
 }
+
+division((1,2)) ; 
+
+
 
 /* Write a function that implement Math.pow(x,n) but using recursion
 Example:
@@ -12,9 +18,12 @@ pow(2,4) = 16
 
 
 function pow(x,n){
-    // Write you logic here.
-    return;
+    if (n == 0)
+    return 1;
+  else
+    return x * pow(x, n - 1);
 }
+
 
 /* The Fibonacci Series is a numeric series starting with the integers 0 and 1. In this series,
 the next integer is determined by summing the previous two. This gives us:
@@ -26,9 +35,16 @@ Write a function that take n as parameter and return the nth element in the Fibo
 Example: n = 4 ==> 3, n= 0 ==> 0, n = 3 ==> 2 */
 
 function fibonacci(n){
-    // Write you logic here.
-    return;
+   if (n == 0){
+    return 0;
+   }
+    else if ( n == 1 ){
+        return 1;
+    } else {
+        return fibonacci(n-1)+fibonacci(n-2)
+    }
 }
+
 
 /* Optional 
 
@@ -53,7 +69,6 @@ function permutations(n,k){
     // Write you logic here. 
     return arr
 };
-
 
 describe("Test division", () => {
     test("Return the division result", () => {
